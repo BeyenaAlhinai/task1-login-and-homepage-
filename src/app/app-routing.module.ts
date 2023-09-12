b1c485ae76f8e28db598549470dac121be975988
BeyenaAlhinai <Beyenaalhinai@gmail.com>
2023-09-12 11:10:37 +0400

    xx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent},
+  {path:'homepage', component:HomePageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
